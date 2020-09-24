06a7fb26a70724a12e430608d954ced5998d53fb
Alan Donoso Naumczuk <alan.donoso@mercadolibre.com>
2020-09-23 21:48:01 -0300

    Ball prefab improved, fx added

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class Ball : MonoBehaviour
+{
+    public Material blackMaterial;
+    public Material whiteMaterial;
+    public Material player1Material;
+    public Material player2Material;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Player1":
+                SetMaterials(player1Material, whiteMaterial);
+                break;
+            case "Player2":
+                SetMaterials(player2Material, whiteMaterial);
+                break;
+            case "Net1":
+                break;
+            case "Net2":
+                break;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        Invoke(nameof(SetOriginalMaterials), 1);
+    }
+
+    private void SetOriginalMaterials()
+    {
+        SetMaterials(whiteMaterial, blackMaterial);
+    }
+
+    private void SetMaterials(Material primaryMaterial, Material secondaryMaterial)
+    {
+        MeshRenderer meshRenderer = GetComponent<MeshRenderer>();
+        Material[] materials = meshRenderer.materials;
+        materials[0] = primaryMaterial;
+        materials[1] = secondaryMaterial;
+        meshRenderer.materials = materials;
+    }
+}
